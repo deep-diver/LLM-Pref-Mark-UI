09db935d62a8d55e55d0aa2a6b3337d26468903c
Chansung Park <deep.diver.csp@gmail.com>
2023-05-31 02:58:17 +0000

    .

---
+++ styles.py
@@ -0,0 +1,230 @@
+BASIC_STYLE = """
+#col-container {
+  width: 80%;
+  margin: auto;
+}
+
+#top-title, #middle-title, #bottom-title {
+  text-align: center;
+}
+
+#prompt-txt-input > label > span {
+  display: none;
+}
+
+#middle-right {
+  border-style: dotted;
+  border-width: 1px;
+  border-radius: 10px;
+  padding: 10px;  
+}
+
+#middle-right-bottom {
+  display: flex;
+  gap: 0px;
+}
+
+#middle-right-bottom > button {
+  box-shadow: none;
+  border-radius: 0px;  
+
+  height: 50px;
+  padding: 0px;
+  width: 50px;
+  min-width: 50px;  
+}
+
+#res-pref-first-btn {
+  border-bottom-left-radius: 10px !important;
+  border-top-left-radius: 10px !important;
+
+  font-size: 1.7vw;
+}
+
+#res-pref-second-btn {
+  font-size: 1.4vw;
+}
+
+#res-pref-third-btn {
+  font-size: 1.1vw;
+}
+
+#res-pref-fourth-btn {
+  font-size: 0.8vw;
+}
+
+#res-pref-fifth-btn {
+  font-size: 0.8vw;
+}
+
+#res-pref-sixth-btn {
+  font-size: 1.1vw;
+}
+
+#res-pref-seventh-btn {
+  font-size: 1.4vw;
+}
+
+#res-pref-eighth-btn {
+  border-bottom-right-radius: 10px !important;
+  border-top-right-radius: 10px !important;
+
+  font-size: 1.7vw;
+}
+
+"""
+
+ADVANCED_STYLE = """
+#col-container {
+  width: 80%;
+  margin: auto;
+}
+
+#top-title, #middle-title, #bottom-title {
+  text-align: center;
+}
+
+#prompt-txt-input > label > span {
+  display: none;
+}
+
+#chatbot {
+    height: 920px; 
+    overflow: auto;
+}
+
+#chatbot > .wrap {
+    max-height: 870px;
+}
+
+#middle-right {
+  border-style: dotted;
+  border-width: 1px;
+  border-radius: 10px;
+  padding: 10px;  
+}
+
+#bottom {
+  border-style: dotted;
+  border-width: 1px;
+  border-radius: 10px;
+  padding: 10px;    
+}
+
+#middle-right-bottom {
+  display: flex;
+  gap: 0px;
+}
+
+#middle-right-bottom > button {
+  box-shadow: none;
+  border-radius: 0px;  
+
+  height: 50px;
+  padding: 0px;
+  width: 50px;
+  min-width: 50px;  
+}
+
+#res-pref-first-btn {
+  border-bottom-left-radius: 10px !important;
+  border-top-left-radius: 10px !important;
+
+  font-size: 1.7vw;
+}
+
+#res-pref-second-btn {
+  font-size: 1.4vw;
+}
+
+#res-pref-third-btn {
+  font-size: 1.1vw;
+}
+
+#res-pref-fourth-btn {
+  font-size: 0.8vw;
+}
+
+#res-pref-fifth-btn {
+  font-size: 0.8vw;
+}
+
+#res-pref-sixth-btn {
+  font-size: 1.1vw;
+}
+
+#res-pref-seventh-btn {
+  font-size: 1.4vw;
+}
+
+#res-pref-eighth-btn {
+  border-bottom-right-radius: 10px !important;
+  border-top-right-radius: 10px !important;
+
+  font-size: 1.7vw;
+}
+
+
+#bottom-bottom {
+  display: flex;
+  gap: 0px;
+  width: 90%;
+  margin: auto;
+}
+
+#bottom-bottom > button {
+  box-shadow: none;
+  border-radius: 0px;  
+
+  height: 50px;
+  padding: 0px;
+  width: 50px;
+  min-width: 50px;  
+}
+
+#chat-pref-first-btn {
+  border-bottom-left-radius: 10px !important;
+  border-top-left-radius: 10px !important;
+
+  font-size: 0.7vw;
+}
+
+#chat-pref-second-btn {
+  font-size: 0.8vw;
+}
+
+#chat-pref-third-btn {
+  font-size: 0.9vw;
+}
+
+#chat-pref-fourth-btn {
+  font-size: 1.0vw;
+}
+
+#chat-pref-fifth-btn {
+  font-size: 1.1vw;
+}
+
+#chat-pref-sixth-btn {
+  font-size: 1.2vw;
+}
+
+#chat-pref-seventh-btn {
+  font-size: 1.3vw;
+}
+
+#chat-pref-eighth-btn {
+  font-size: 1.4vw;
+}
+
+#chat-pref-nineth-btn {
+  font-size: 1.5vw;
+}
+
+#chat-pref-tenth-btn {
+  border-bottom-right-radius: 10px !important;
+  border-top-right-radius: 10px !important;
+
+  font-size: 1.7vw;
+}
+"""
